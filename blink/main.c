d1d7e23bce1f8fcfca069e54d1f4f7929f3bd1d3
fysnet <spamcollector@cableone.net>
2015-04-04 12:50:01 -0700

    Update main.c

--- blink/main.c
+++ blink/main.c
@@ -1,3 +1,22 @@
+/////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 1984-2015  Benjamin D Lunt (fys [at] fysnet [dot] net)
+//
+//  This firmware is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This firmware is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+/////////////////////////////////////////////////////////////////////////
+
 // http://www.fysnet.net/attiny2313.htm
 #include <avr/io.h>
 #include <avr/interrupt.h>  // for sei()
